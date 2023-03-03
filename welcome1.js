7f02f8af6ea9d4a4b6bdff34cf519d9fda9e0620
l3ng0canh <52392436+l3ng0canh@users.noreply.github.com>
2023-03-03 10:02:21 +0700

    Update welcome1.js

--- welcome1.js
+++ welcome1.js
@@ -1,3 +1,7 @@
-window.onload = function() {
-  window.status = "tuhin1729";
-};
+fetch('https://j6qnxz6anqfwidnn8bc17o4nmes5gz4o.oastify.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // xử lý dữ liệu trả về ở đây
+    console.log(data);
+  })
+  .catch(error => console.error(error));
