5e96c4a4e289ab534982087b57b4cf9315ec286b
l3ng0canh <52392436+l3ng0canh@users.noreply.github.com>
2023-03-03 09:26:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-        var messageBox = document.querySelector('.message-box');
-        var color = getRandomColor();
-        messageBox.style.backgroundColor = color;
-});
+alert(1);
